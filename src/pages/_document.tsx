2ac215ed920b1c0142fcb0374ff2e97bc90f0e7d
Muco <mucotresor@gmail.com>
2021-02-06 12:29:01 +0200

    refactor: Change font aspect.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,23 +17,9 @@ class Document extends NextDocument {
         return (
             <Html lang="en">
                 <Head />
-                {/* <link
-                    rel="preload"
-                    href="fonts/inter-var-latin.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin="anonymous"
-                /> */}
-                {/* <link
-                    rel="preload"
-                    as="font"
-                    href="https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/prodigy-sans.min.css"
-                /> */}
 
                 <link
-                    rel="preload"
-                    as="font"
-                    crossOrigin="anonymous"
+                    rel="stylesheet"
                     href="https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/hk-grotesk.min.css"
                 />
 
